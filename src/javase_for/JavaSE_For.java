025ee116aaea427c60626cb8a9469852ec34554f
Besim Öznalçin <besim437@hotmail.com>
2017-06-09 00:04:02 +0300

    Add files via upload

---
+++ src/javase_for/JavaSE_For.java
@@ -0,0 +1,17 @@
+
+package javase_for;
+
+public class JavaSE_For {
+
+    public static void main(String[] args) {
+        
+        int dizi[]={11,22,33,44};
+        for(int i:dizi)
+            System.out.println(i);
+        
+        String dizi2[] ={"Mehmet","Besim","Emir"};
+        for (String i:dizi2)
+            System.out.println(i);
+    }
+    
+}
